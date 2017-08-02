56d23eef407dbbd6aff50e805380eda6c3c1dbeb
bddowningjennings-dev <bddowningjennings.dev@gmail.com>
2017-08-02 16:40:48 -0700

    updated underscore.py

--- Python/OOP/underscore.py
+++ Python/OOP/underscore.py
@@ -15,24 +15,19 @@ class Underscore(object):
     for el in arr:
       if cb(el):
         return el
-  def reduce(self, arr, cb):
+  def reduce(self, arr, cb, memo = 0,):
     temp = 0
     for el in arr:
-      temp += cb(el)
+      temp += cb(memo, el)      
     return temp
-  # def map(self, arr, cb):
-  #   temp = []
-  #   for el in arr:
-  #     temp.append(cb(el))
-  #   return temp
-  # def map(self, arr, cb):
-  #   temp = []
-  #   for el in arr:
-  #     temp.append(cb(el))
-  #   return temp
-  
+  def reject(self, arr, cb):
+    temp = []
+    for el in arr:
+      if not cb(el):
+        temp.append(el)
+    return temp
 
 _ = Underscore()
-# evens = _.reduce([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
-evens = _.reduce([1, 2, 3], lambda memo = 0, num: memo + num);
+evens = _.reject([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
+# evens = _.reduce([1, 2, 3], lambda memo, num: memo + num, 0);
 print evens
