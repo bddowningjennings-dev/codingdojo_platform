70fb4c5526a5c46c1a44928c42857770d5a03db1
bddowningjennings-dev <bddowningjennings.dev@gmail.com>
2017-08-04 16:09:10 -0700

    added form submission demo

---
+++ Python/python_stack/myEnvironments/whatsamahname/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+  print request.form['name']
+  return redirect('/')
+
+app.run(debug=True)
