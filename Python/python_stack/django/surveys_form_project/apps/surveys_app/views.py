1b3f7d4d4151a8aa7968d9419fcf4aca10f6d929
bddowningjennings-dev <bddowningjennings.dev@gmail.com>
2017-08-11 16:27:30 -0700

    added surveys_form_project

--- Python/python_stack/django/surveys_form_project/apps/surveys_app/views.py
+++ Python/python_stack/django/surveys_form_project/apps/surveys_app/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 
 def index(request):
     # request.session['count'] = 0
